b392532dd5b9b15f5b3a826164f3adcda1c1d797
Vladislav Zubtcs <48099338+vladislavzubetcs@users.noreply.github.com>
2021-03-15 03:31:38 +0300

    php integration

---
+++ api.php
@@ -0,0 +1,24 @@
+<?php
+
+try {
+    $dbh = new PDO('mysql:host=localhost;dbname=artasurb_db', 'artasurb_db_user', 'db_pass');
+
+    $billboards = array();
+    $i = 0;
+
+    foreach($dbh->query('SELECT * from billsboards') as $row) {
+        $billboards[$i]['id'] = $row['id'];
+        $billboards[$i]['title'] = $row['title'];
+        $billboards[$i]['markCoords'] = $row['markCoords'];
+
+        $i++;
+    }
+
+    print json_encode($billboards);
+
+    $dbh = null;
+
+} catch (PDOException $e) {
+    print json_encode($e);
+    die();
+}
---
+++ callback.php
@@ -0,0 +1,27 @@
+<?php
+
+$data = file_get_contents("php://input");
+
+$fp = fopen('callback_values.txt', 'a');
+fwrite($fp, $data);
+fclose($fp);
+
+$data = json_decode($data, true);
+
+$to  = "vladislavzubetcs@gmail.com, " ; 
+$to .= "<vladislavzubetcs@gmail.com>"; 
+
+$subject = "artasur.by: Заявка на обратный звонок"; 
+
+$billboards_string = false;
+if(!empty($data['billboards'])) {
+  $billboards_string = "Выбранные рекламные щиты: \n";
+
+  foreach($data['billboards'] as $billboard)
+    $billboards_string .= $billboard['title'] . "\n";
+
+}
+
+$message = "Заявка на обратный звонок с сайта artasur.by\n\nОтправил заявку:\n{$data['name']}\n\nEmail отправителя:\n{$data['email']}\n\nТелефон отправителя:\n{$data['phone']}\n\n\n" . ($billboards_string ? $billboards_string:'Рекламные щиты не выбраны.');
+
+mail($to, $subject, $message);
