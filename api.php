8b6039c850d2ae219b8536d3d71646c487999d8b
vladislavzubetcs <vladislavzubetcs@gmail.com>
2021-04-11 22:30:13 +0300

    admin panel php api release

--- api.php
+++ api.php
@@ -1,33 +1,110 @@
 <?php
 
-try {
-    $dbh = new PDO('mysql:host=localhost;dbname=artasurb_db', 'artasurb_db_user', 'db_pass');
+$data = file_get_contents("php://input");
 
-    $billboards;
-    $i = 0;
+$fp = fopen('api_values.txt', 'a');
+fwrite($fp, $data);
+fclose($fp);
 
-    foreach($dbh->query('SELECT * from billboards') as $row) {
-        $billboards[$i]['id'] = $row['id'];
-        $billboards[$i]['title'] = $row['title'];
+switch ($data['action']) {
+    case 'all':
+        getAll();
+        break;
+
+    case 'get':
+        getById($data['data']);
+        break;
+
+    case 'insert':
+        insert($data['data']['title'], $data['data']['url']);
+        break;
+
+    case 'update':
+        update($data['data']['id'], $data['data']['title'], $data['data']['url']);
+        break;
+
+    case 'delete':
+        delete($data['data']['id']);
+        break;
+    
+    default:
+        header("HTTP/1.1 500 Internal Server Error", true, 500);
+        break;
+}
+
+function getAll() {
+    try {
+        $dbh = new PDO('mysql:host=localhost;dbname=artasurb_db', 'artasurb_db_user', 'db_pass');
+
+        $billboards = array();
+        $i = 0;
+
+        foreach($dbh->query('SELECT * from billboards') as $row) {
+            $billboards[$i]['id'] = $row['id'];
+            $billboards[$i]['title'] = $row['title'];
+
+            $matches = array();
+            preg_match_all('/[\d.]+/', $row['markCoords'], $matches);
+            $billboards[$i]['markCoords'] = $matches[0];
+
+            // preg_match_all('/[\d.]+/', $row['markDirection'], $matches);
+            $billboards[$i]['markDirection'] = $matches[0];
+
+            // preg_match_all('/[\d.]+/', $row['markSpan'], $matches);
+            $billboards[$i]['markSpan'] = array(0,0);
+
+            $i++;
+        }
+
+        print json_encode($billboards, JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK);
+
+        $dbh = null;
+
+    } catch (PDOException $e) {
+        header("HTTP/1.1 500 Internal Server Error", true, 500);
+        return true;
+    }
+
+    header("HTTP/1.1 200 OK", true, 200);
+    return true;
+}
+
+function getById($id) {
+    try {
+        $dbh = new PDO('mysql:host=localhost;dbname=artasurb_db', 'artasurb_db_user', 'db_pass');
+
+        $sql = 'SELECT * from billboards WHERE id = :id';
+
+        $result = $db->prepare($sql);
+        $result->bindParam(':id', $id, PDO::PARAM_INT);
+        $result->setFetchMode(PDO::FETCH_ASSOC);
+        $result->execute();
+
+        $billboard = array();
+        $billboards['id'] = $row['id'];
+        $billboards['title'] = $row['title'];
 
         $matches = array();
         preg_match_all('/[\d.]+/', $row['markCoords'], $matches);
-        $billboards[$i]['markCoords'] = $matches[0];
+        $billboards['markCoords'] = $matches[0];
 
-        // preg_match_all('/[\d.]+/', $row['markDirection'], $matches);
-        $billboards[$i]['markDirection'] = $matches[0];
+        preg_match_all('/[\d.]+/', $row['markDirection'], $matches);
+        $billboards['markDirection'] = $matches[0];
 
-        // preg_match_all('/[\d.]+/', $row['markSpan'], $matches);
-        $billboards[$i]['markSpan'] = array(0,0);
+        preg_match_all('/[\d.]+/', $row['markSpan'], $matches);
+        $billboards['markSpan'] = $matches[0];
 
-        $i++;
+        print json_encode($billboard, JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK);
+
+        $dbh = null;
+
+    } catch (PDOException $e) {
+        header("HTTP/1.1 500 Internal Server Error", true, 500);
+        return true;
     }
 
-    print json_encode($billboards, JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK);
-
-    $dbh = null;
-
-} catch (PDOException $e) {
-    print json_encode($e);
-    die();
+    header("HTTP/1.1 200 OK", true, 200);
+    return true;
 }
+
+die();
